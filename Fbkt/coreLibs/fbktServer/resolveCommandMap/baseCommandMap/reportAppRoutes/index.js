1691ec0b2059c165d3e52a097775cfefb686cf67
stlbucket <stlbucket@gmail.com>
2017-02-13 16:45:08 -0600

    1.0.56

--- Fbkt/coreLibs/fbktServer/resolveCommandMap/baseCommandMap/reportAppRoutes/index.js
+++ Fbkt/coreLibs/fbktServer/resolveCommandMap/baseCommandMap/reportAppRoutes/index.js
@@ -4,5 +4,5 @@ var fbkt = require('../../../../../../Fbkt');
 module.exports = function(){
 	fbkt().clog('APP ROUTES', _.filter(fbkt().restApiRoutes, function(appRoute){
 		return true;
-	}));
+	}), true);
 };
