ebeec298e4d2ca63147b5675a14850164a891cd8
stlbucket <stlbucket@gmail.com>
2017-02-14 14:40:41 -0600

    check for -env and for file exists

--- config/index.js
+++ config/index.js
@@ -1,18 +1,6 @@
-// const args = require('minimist')(process.argv.slice(2));
-// const NODE_ENV = args.NODE_ENV;
-// const envPath = `./.env.${NODE_ENV}`;
-// process.env.NODE_ENV = NODE_ENV;
-//
-// require('dotenv-safe').load({
-//   allowEmptyValues: false,
-//   sample: './.env.example',
-//   path: envPath
-// });
-
-
 module.exports = {
+  port: process.env.PORT || 20831,
   appRouteFilter: process.env.APP_ROUTE_FILTER,
   restErrorMode: process.env.REST_ERROR_MODE,
-  defaultEntityControllerAuth: process.env.DEFAULT_ENTITY_CONTROLLER_AUTH,
-  port: process.env.PORT || 20831
+  defaultEntityControllerAuth: process.env.DEFAULT_ENTITY_CONTROLLER_AUTH
 };
--- loadEnv/index.js
+++ loadEnv/index.js
@@ -1,10 +1,21 @@
+const colors = require('colors');
 const args           = require('minimist')(process.argv.slice(2));
 const NODE_ENV       = args.NODE_ENV;
 const envPath        = `./.env.${NODE_ENV}`;
 process.env.NODE_ENV = NODE_ENV;
 
-require('dotenv-safe').load({
-  allowEmptyValues: false,
-  sample: './.env.example',
-  path: envPath
-});
+if (NODE_ENV === null || NODE_ENV === undefined) {
+  console.log(colors.red('YOU MUST SPECIFY --NODE_ENV ON THE COMMAND LINE'));
+  process.exit();
+}
+
+try {
+  require('dotenv-safe').load({
+    allowEmptyValues: false,
+    sample: './.env.example',
+    path: envPath
+  });
+} catch (error) {
+  console.log(colors.red(`ENV FILE DOES NOT EXIST: ${envPath}`));
+  process.exit();
+}
