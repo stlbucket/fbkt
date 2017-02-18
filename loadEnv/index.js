49c1d338f65677aa8c178110e4dc0e7107294a46
stlbucket <stlbucket@gmail.com>
2017-02-18 06:58:05 -0600

    1.0.70

--- loadEnv/index.js
+++ loadEnv/index.js
@@ -1,8 +1,10 @@
 const colors = require('colors');
 const args           = require('minimist')(process.argv.slice(2));
-const NODE_ENV       = args.NODE_ENV;
+const NODE_ENV       = process.env.NODE_ENV || args.NODE_ENV;
 const envPath        = `./.env.${NODE_ENV}`;
 process.env.NODE_ENV = NODE_ENV;
+
+console.log('NODE_ENV', NODE_ENV);
 
 if (NODE_ENV === null || NODE_ENV === undefined) {
   console.log(colors.red('YOU MUST SPECIFY --NODE_ENV ON THE COMMAND LINE'));
