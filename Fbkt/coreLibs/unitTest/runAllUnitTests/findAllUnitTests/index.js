34166ec3642ee62c428f9e1f68e447823ef4f74c
stlbucket <stlbucket@gmail.com>
2017-02-21 14:30:54 -0600

    testLib

--- Fbkt/coreLibs/unitTest/runAllUnitTests/findAllUnitTests/index.js
+++ Fbkt/coreLibs/unitTest/runAllUnitTests/findAllUnitTests/index.js
@@ -1,34 +1,37 @@
 "use strict";
-var R = require('ramda');
-var Promise = require('bluebird');
-var fbkt = require('../../../../../Fbkt');
+var R                = require('ramda');
+var Promise          = require('bluebird');
+var fbkt             = require('../../../../../Fbkt');
 var findLibUnitTests = require('../findLibUnitTests');
+const args           = require('minimist')(process.argv.slice(2));
 
-module.exports = (callInfo)=>{
-	return fbkt().FbktPipe({
-		name:           'findAllUnitTests',
-		filename:       __filename,
-		pipelineSteps: {  // any number of functions
-			"findAllUnitTests": function (callInfo) {
-				const libs = fbkt().libs;
-				let libUnitTestSets = [];
-				R.forEach((lib)=>{
-					const libRelativePath = R.is(Function, libs[lib].libRelativePath) ? libs[lib].libRelativePath() : null;
-					const ignoreLib = R.is(String, callInfo.params.testLibName) && (lib !== callInfo.params.testLibName);
-					if (libRelativePath && !ignoreLib) {
-						libUnitTestSets.push(findLibUnitTests().execute({
-							params: {
-								libRelativePath: libRelativePath
-							}
-						}));
-					}
-			}, R.keys(libs));
+module.exports = (callInfo)=> {
+  return fbkt().FbktPipe({
+    name: 'findAllUnitTests',
+    filename: __filename,
+    pipelineSteps: {  // any number of functions
+      "findAllUnitTests": function (callInfo) {
+        const testLib = args.testLib;
+        const libs    = R.isNil(testLib) ? fbkt().libs : {[testLib]: fbkt().libs[testLib]};
 
-				return Promise.reduce(libUnitTestSets, function(all, utSet){
-					return R.concat(all, utSet);
-				}, []);
+        let libUnitTestSets = [];
+        R.forEach((lib)=> {
+          const libRelativePath = R.is(Function, libs[lib].libRelativePath) ? libs[lib].libRelativePath() : null;
+          const ignoreLib       = R.is(String, callInfo.params.testLibName) && (lib !== callInfo.params.testLibName);
+          if (libRelativePath && !ignoreLib) {
+            libUnitTestSets.push(findLibUnitTests().execute({
+              params: {
+                libRelativePath: libRelativePath
+              }
+            }));
+          }
+        }, R.keys(libs));
 
-			}
-		}
-	}, callInfo);
+        return Promise.reduce(libUnitTestSets, function (all, utSet) {
+          return R.concat(all, utSet);
+        }, []);
+
+      }
+    }
+  }, callInfo);
 };
