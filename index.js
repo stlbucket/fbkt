3248a78b61444ff00b64d84977be59a89650dbae
stlbucket <stlbucket@gmail.com>
2017-03-15 07:28:26 -0500

    1.0.90

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('./loadEnv');
 let __fbkt = null;
 const Fbkt = require('./Fbkt');
 const baseCommandMap = require('./Fbkt/coreLibs/fbktServer/resolveCommandMap/baseCommandMap');
+const graphql = require('graphql').graphql;
 
 module.exports = (config, appLibs, getBaseCommandMap)=>{
   const useConfig = config || { noConfig: true };
@@ -10,3 +11,10 @@ module.exports = (config, appLibs, getBaseCommandMap)=>{
 };
 
 module.exports.baseCommandMap = baseCommandMap;
+
+module.exports.queryGraphQl = (query) => {
+  return graphql(fbkt().graphqlSchema, graphQlQuery)
+    .then(result => {
+      return result.data;
+    });
+};
