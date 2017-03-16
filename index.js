e6a81ba64cce99dc9e13758b15c5b2b8a616abda
stlbucket <stlbucket@gmail.com>
2017-03-16 13:44:19 -0500

    2.0.10

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('./loadEnv');
-let __fbkt = null;
-const Fbkt = require('./Fbkt');
+let __fbkt           = null;
+const Fbkt           = require('./Fbkt');
 const baseCommandMap = require('./Fbkt/coreLibs/fbktServer/resolveCommandMap/baseCommandMap');
-const graphql = require('graphql').graphql;
-const graphqlSchema = require('./Fbkt/graphqlSchema');
+const graphql        = require('graphql').graphql;
+const graphqlSchema  = require('./Fbkt/graphqlSchema');
 
-module.exports = (config, appLibs, getBaseCommandMap)=>{
-  const useConfig = config || { noConfig: true };
-  __fbkt = Fbkt(useConfig, appLibs);
+module.exports = (config, appLibs, getBaseCommandMap) => {
+  const useConfig = config || {noConfig: true};
+  __fbkt          = Fbkt(useConfig, appLibs);
   return __fbkt;
 };
 
@@ -16,7 +16,15 @@ module.exports.baseCommandMap = baseCommandMap;
 module.exports.queryGraphql = (graphQlQuery) => {
   return graphql(graphqlSchema(), graphQlQuery)
     .then(result => {
-      return result.data;
+      if (result.errors && result.errors.length > 0) {
+        Fbkt().clog('GRAPH QL QUERY ERRORS', result.errors, true);
+      } else {
+        return result.data;
+      }
+    })
+    .catch(error => {
+      Fbkt().clog('GRAPH QL QUERY ERROR', error, true);
+      throw error;
     });
 };
 
